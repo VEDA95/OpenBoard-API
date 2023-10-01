c1226bc69a8bdcfaa53e09c9d4ec6cc31b268404
Stefan Netterfield <stefan@5f03.com>
2023-09-30 22:55:32 -0400

    Created plugin for db pool connecttion...

---
+++ src/db/index.ts
@@ -0,0 +1,31 @@
+import { Pool } from 'pg';
+import fp from 'fastify-plugin';
+import type { FastifyInstance, FastifyServerOptions } from 'fastify';
+import type { PoolClient } from 'pg';
+
+export interface DBOptions extends FastifyServerOptions {
+    db_uri: string;
+    connection_max?: number;
+}
+
+export default fp(async (fastify: FastifyInstance, options: DBOptions): Promise<void> => {
+    const pool: Pool = new Pool({connectionString: options.db_uri, max: options.connection_max ?? 10});
+
+    pool.on('error', (err: Error): void => {
+        console.error('Unexpected error on idle client', err);
+        process.exit(-1);
+    });
+
+    try {
+        const client: PoolClient = await pool.connect();
+
+        fastify.decorate('db', client);
+        return;
+
+    } catch(err: any) {
+        if(err != null) {
+            console.error('Unexpected error when connecting to database', err);
+            process.exit(-1);
+        }
+    }
+});
