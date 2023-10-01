b0264f81bb949475d3cb5382027a4694d17d6398
Stefan Netterfield <stefan@5f03.com>
2023-10-01 06:37:56 -0400

    Added proper types to callbacks...

--- tests/hello.ts
+++ tests/hello.ts
@@ -1,13 +1,14 @@
 import { assert, use, request } from 'chai';
 import chaiHttp from 'chai-http';
 
+
 use(chaiHttp);
 
 describe('/ route', (): void => {
-    it('Testing hello world GET request.', (done): void => {
+    it('Testing hello world GET request.', (done: Mocha.Done): void => {
         request('http://localhost:3030')
             .get('/')
-            .end((err, response): void => {
+            .end((err: any, response: ChaiHttp.Response): void => {
                 assert.equal(response.text, 'Hello World!');
                 done();
             });
