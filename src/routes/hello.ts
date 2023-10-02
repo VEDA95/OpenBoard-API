d67d398b28d007c9c2bd4cdd0767c1cfa0d86672
Stefan Netterfield <stefan@5f03.com>
2023-10-01 20:44:02 -0400

    Refactored hello world test to run properly...

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -3,7 +3,7 @@ import type { DoneCallback } from './types/done';
 
 export default (fastify: FastifyInstance, options: FastifyServerOptions, done: DoneCallback): void => {
     fastify.get('/', async (request: FastifyRequest, response: FastifyReply): Promise<string> => {
-        return 'Hello world!';
+        return 'Hello World!';
     });
 
     done();
--- tests/hello.ts
+++ tests/hello.ts
@@ -6,7 +6,7 @@ use(chaiHttp);
 
 describe('/ route', (): void => {
     it('Testing hello world GET request.', (done: Mocha.Done): void => {
-        request('http://localhost:3030')
+        request('http://localhost:3030/api')
             .get('/')
             .end((err: any, response: ChaiHttp.Response): void => {
                 assert.equal(response.text, 'Hello World!');
